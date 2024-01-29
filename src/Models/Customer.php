9d477ef9bafe6092e962e6c27e4f6d3b8e4ebb29
Karim Mohamed <komicho1996@gmail.com>
2024-01-29 08:42:53 +0200

    Feat: create the `Customer` model

---
+++ src/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Basketin\Component\Customers\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Customer extends Authenticatable
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'password',
+    ];
+}
