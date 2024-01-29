5865d35476c2ae6d48762e45d87914f79596ebab
Karim Mohamed <komicho1996@gmail.com>
2024-01-29 08:41:02 +0200

    Feat: create the service provider

---
+++ src/Providers/BasketinCustomersServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Basketin\Component\Customers\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BasketinCustomersServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
